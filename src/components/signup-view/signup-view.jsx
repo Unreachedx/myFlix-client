9857d5fdb3a6355770d44452ded3bef09ec79245
Patrick <114505469+Unreachedx@users.noreply.github.com>
2024-07-01 02:56:44 +0200

    updated signup

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -16,7 +16,7 @@ export const SignupView = () => {
       Birthday: birthday,
     };
 
-    fetch("http://localhost:8080/users", {
+    fetch("https://myflixapplication-paddy-fac687c8aed3.herokuapp.com/users", {
       method: "POST",
       body: JSON.stringify(data),
       headers: {
